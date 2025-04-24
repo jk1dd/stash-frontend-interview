c34ccdd9ff7dd3307af194d6f06cae442df9d3ae
jk1dd <portajonk@gmail.com>
2025-04-24 13:16:31 +0100

    add zustand stores

---
+++ src/stores/useHotelStore.ts
@@ -0,0 +1,30 @@
+import { create } from "zustand"
+// import data from "../data/data.json"
+
+export type Hotel = {
+  name: string
+  city: string
+  daily_rate: number
+  id: number
+  has_member_rate: boolean
+  image: string
+}
+
+type HotelState = {
+  hotels: Hotel[];
+  fetchHotels: () => Promise<void>;
+  getHotelById: (id: number) => Hotel | undefined;
+};
+
+export const useHotelStore = create<HotelState>((set, get) => ({
+  hotels: [],
+  fetchHotels: async () => {
+    const response = await fetch('/data.json');
+    const data = await response.json();
+    set({ hotels: data });
+  },
+  getHotelById: (id) => {
+    const { hotels } = get();
+    return hotels.find((hotel) => hotel.id === id);
+  },
+}));
---
+++ src/stores/useSearchStore.ts
@@ -0,0 +1,26 @@
+import { create } from 'zustand';
+
+type SearchParams = {
+  city: string;
+  checkin: string;
+  checkout: string;
+  adults: number;
+  children: number;
+};
+
+type SearchState = {
+  searchParams: SearchParams;
+  updateSearchParams: (params: Partial<SearchParams>) => void;
+};
+
+export const useSearchStore = create<SearchState>((set) => ({
+  searchParams: {
+    city: '',
+    checkin: '',
+    checkout: '',
+    adults: 1,
+    children: 0,
+  },
+  updateSearchParams: (params) =>
+    set((state) => ({ searchParams: { ...state.searchParams, ...params } })),
+}));
